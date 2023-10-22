059c0d6d44a1c759db26f9a5ebfe94e88b316044
twinjke <seventeensasha7@gmail.com>
2023-10-22 12:45:28 +0300

    feat: add-brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const len = str.length / 2;
+  const pairs = bracketsConfig
+    .flat()
+    .join()
+    .replaceAll(",", "")
+    .match(/.{1,2}/g);
+  for (let i = 0; i < len; i++) {
+    for (let pair of pairs) {
+      if (str.includes(pair)) {
+        str = str.replace(pair, "");
+      }
+    }
+  }
+
+  return str ? false : true;
+};
